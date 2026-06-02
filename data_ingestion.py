c894c7f3a89c0949982da8832910fe6fa4283275
Yukta1918 <sutharyukta1920@gmail.com>
2026-06-02 17:58:19 +0530

    Day 1: Data ingestion complete

---
+++ data_ingestion.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+folder_path = "data/raw"
+
+csv_files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
+
+for file in csv_files:
+    print("\n" + "="*60)
+    print("Dataset:", file)
+
+    df = pd.read_csv(os.path.join(folder_path, file))
+
+    print("\nShape:")
+    print(df.shape)
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print("\nFirst 5 Rows:")
+    print(df.head())
+
+    # print("\nMissing Values:")
+    # print(df.isnull().sum())
+
+    # print("Duplicates:", df.duplicated().sum())
+
+
+#Any anomalies
+#1. 12 Missing values found 04_monthly_sip_inflows in yoy_growth_pct column.
---
+++ live_nav_fetch.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+import os
+
+schemes = {
+    "HDFC_Top100":125497,
+    "SBI_Bluechip":119551,
+    "ICICI_Bluechip":120503,
+    "Nippon_LargeCap":118632,
+    "Axis_Bluechip":119092,
+    "Kotak_Bluechip":120841
+}
+
+save_folder = "data/raw"
+
+for name, code in schemes.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        df = pd.DataFrame(data["data"])
+
+        df.to_csv(
+            os.path.join(save_folder, f"{name}.csv"),
+            index=False
+        )
+
+        print(f"Saved {name}")
